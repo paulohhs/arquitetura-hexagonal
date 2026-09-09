dcccd395083a43e6471ca96e9ebff5891b4c7a24
paulohhs <paulohhonoratos@gmail.com>
2026-09-09 18:38:09 -0300

    adapters(adapters/db/product_test.go): criando teste para adaptador db.

---
+++ adapters/db/product_test.go
@@ -0,0 +1,56 @@
+package db_test
+
+import (
+	"database/sql"
+	"log"
+	"testing"
+
+	"github.com/paulohhs/arquitetura-hexagonal/adapters/db"
+	"github.com/stretchr/testify/require"
+)
+
+var Db *sql.DB
+
+func setup() {
+	Db, _ = sql.Open("sqlite3", ":memory:")
+	createTable(Db)
+	createProduct(Db)
+}
+
+func createTable(db *sql.DB) {
+	table := `CREATE TABLE products (
+			"id" string,
+			"name" string,
+			"price" float,
+			"status" string
+			);`
+
+	stmt, err := db.Prepare(table)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	stmt.Exec()
+}
+
+func createProduct(db *sql.DB) {
+	insert := `insert into products values("abc", "Product Test", 0, "disabled");`
+	stmt, err := db.Prepare(insert)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	stmt.Exec()
+}
+
+func TestProductDb_Get(t *testing.T) {
+	setup()
+	defer Db.Close()
+
+	prodcutDb := db.NewProductDb(Db)
+	product, err := prodcutDb.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, "Product Test", product.GetName())
+	require.Equal(t, 0.0, product.GetPrice())
+	require.Equal(t, "disabled", product.GetStatus())
+}
