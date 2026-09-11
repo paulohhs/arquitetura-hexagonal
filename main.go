436f1f77c56695ca21b4beec0fe18e2b916dd3f2
paulohhs <paulohhonoratos@gmail.com>
2026-09-10 22:18:51 -0300

    refactor(main.go): adicionando multiplas bases para atender multiplos adapters.

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
-/*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
-*/
 package main
 
+/*
+Base main para testar CLI e WEBSERVER adapters
+*/
 import "github.com/paulohhs/arquitetura-hexagonal/cmd"
 
 func main() {
 	cmd.Execute()
 }
+
+/*
+Base main para testar DB adapter
+*/
+// import (
+// 	"database/sql"
+// 	db2 "github.com/paulohhs/arquitetura-hexagonal/adapters/db"
+// 	"github.com/paulohhs/arquitetura-hexagonal/application"
+// )
+
+// func main() {
+// 	db, _ := sql.Open("sqlite3", "db.sqlite")
+// 	productDbAdapter := db2.NewProductDb(db)
+// 	productService := application.NewProductService(productDbAdapter)
+
+// 	product, _ := productService.Create("Produto Exemplo", 30)
+// 	productService.Enable(product)
+// 	productService.Disable(product)
+// }
